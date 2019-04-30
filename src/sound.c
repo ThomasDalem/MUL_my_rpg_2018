98d8a079c12d4e0297a3ace18bd7b63840bd6fa8
Roméo <romeo@localhost.localdomain>
2019-04-30 08:28:58 -0400

    [ADD] Sound for pnj's talk

--- src/sound.c
+++ src/sound.c
@@ -15,9 +15,9 @@ void talk(scene_t *scene)
     y = rand() % 3;
     switch (y) {
         printf("[%d]\n", y);
-    case 1: sfMusic_play(scene->pnj->talk->sound); break;
-    case 2: printf("talk 2\n"); break;
-    case 3: printf("talk 3\n"); break;
+    case 0: sfMusic_play(scene->pnj->talk->sound); break;
+    case 1: printf("talk 2\n"); break;
+    case 2: printf("talk 3\n"); break;
     default: printf("Unknow Error :/\n");
     }
 }
